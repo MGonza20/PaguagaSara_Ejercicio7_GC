58bacf71ade76eff397c9c62d806c9367f98436a
MGonza20 <sara.paguaga@gmail.com>
2022-09-23 23:07:18 -0600

    adding ior values

--- Raytracer.py
+++ Raytracer.py
@@ -12,8 +12,8 @@ marble1 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex.bm
 marble2 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex-2.bmp"), spec = 32, matType = REFLECTIVE) 
 marble3 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex-3.bmp"), spec = 32, matType = OPAQUE) 
 marble4 = Material( diffuse = (1, 1, 1), texture = Texture("colored-tex-6.bmp"), spec = 32, matType = OPAQUE) 
-marble5 = Material( diffuse = (171/255, 240/255, 1), texture = Texture("colored-tex-8.bmp"), spec = 32, matType = TRANSPARENT) 
-marble6 = Material( diffuse = (247/255, 95/255, 20/255), texture = Texture("colored-tex-8.bmp"), spec = 32, matType = TRANSPARENT) 
+marble5 = Material( diffuse = (171/255, 240/255, 1), texture = Texture("colored-tex-8.bmp"), spec = 32,  ior = 1.5, matType = TRANSPARENT) 
+marble6 = Material( diffuse = (247/255, 95/255, 20/255), texture = Texture("colored-tex-8.bmp"), spec = 32,  ior = 2.417, matType = TRANSPARENT) 
 
 
 rtx = Raytracer(width, height)
