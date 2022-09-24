fb8f54b47b9ffbdfd377a32869c7a8c3d14aebdb
MGonza20 <sara.paguaga@gmail.com>
2022-09-23 21:37:15 -0600

    Placing balls in scene

--- Raytracer.py
+++ Raytracer.py
@@ -16,6 +16,12 @@ earth = Material(texture = Texture("earthDay.bmp"))
 marble = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("marbleTex2.bmp"), spec = 32, matType = REFLECTIVE)
 marble2 = Material( diffuse = (0.2, 0.2, 0.8), texture = Texture("marbleTex3.bmp"), spec = 32, matType = TRANSPARENT)
 
+marble3 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex.bmp"), spec = 32, matType = REFLECTIVE) # yes
+marble4 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex-2.bmp"), spec = 32, matType = REFLECTIVE) # yes
+marble5 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex-3.bmp"), spec = 32, matType = OPAQUE) # yes
+marble7 = Material( diffuse = (0.8, 0.8, 0.8), texture = Texture("colored-tex-6.bmp"), spec = 32, matType = OPAQUE) # yes
+marble9 = Material( diffuse = (171/255, 240/255, 1), texture = Texture("colored-tex-8.bmp"), spec = 32, matType = TRANSPARENT) # yes
+marble10 = Material( diffuse = (247/255, 95/255, 20/255), texture = Texture("colored-tex-8.bmp"), spec = 32, matType = TRANSPARENT) # yes
 
 mirror = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
 
@@ -33,9 +39,14 @@ rtx.lights.append( AmbientLight(intensity = 0.1 ))
 rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
 #rtx.lights.append( PointLight(point = (0,0,0)))
 
-# rtx.scene.append( Sphere(V3(-3,0,-10), 1, earth))
-rtx.scene.append( Sphere(V3(0,0,-10), 3, marble2))
-# rtx.scene.append( Sphere(V3(3,0,-10), 1, glass))
+rtx.scene.append( Sphere(V3(-3,1.5,-10), 1, marble3))
+rtx.scene.append( Sphere(V3(0,1.5,-10), 1, marble5))
+rtx.scene.append( Sphere(V3(3,1.5,-10), 1, marble9))
+
+rtx.scene.append( Sphere(V3(-3,-1.5,-10), 1, marble4))
+rtx.scene.append( Sphere(V3(0,-1.5,-10), 1, marble7))
+rtx.scene.append( Sphere(V3(3,-1.5,-10), 1, marble10))
+
 # rtx.scene.append( Sphere(V3(3,1,-13), 1, stone))
 
 # rtx.scene.append( Sphere(V3(-3,0,-10), 1, brick))
